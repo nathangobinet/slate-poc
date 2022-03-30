d74dc7ee4647fab8d59934ade7dd90065624d73b
nathangobinet <nathan.gobinet@gmail.com>
2022-03-30 17:24:58 -0400

    Remove useless console log

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
 
   const serialize = () => {
     const editor = getPlateEditorRef('mail-editor');
-    console.log(editor.children);
     const mail = htmlSerialize(editor.children);
     setMailContent(mail);
   };
